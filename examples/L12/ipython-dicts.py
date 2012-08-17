c5dfc2927b2918c839161c57e406bdc108645a04
Matthew Rocklin <mrocklin@gmail.com>
2012-08-17 16:23:23 -0500

    add ipython history about accounts/dicts

---
+++ examples/L12/ipython-dicts.py
@@ -0,0 +1,15 @@
+# This is the history of commands of the ipython session at the end of class
+
+account = ("Alice", 50, True) # representing an account witha tuple
+account[1] # have to index
+account = {}  # make an empty dict
+account["name"] = "Alice" # set key "name" to value "Alice"
+account["balance"] = 50
+account["trusted"] = True
+account["balance"] # get value with key "balance"
+print account
+account = {"name": "Alice", "balance": 50, "trusted":True} #other syntax
+account
+account["name"]
+stimulus_project = {"city":"Chicago", "cost":50000000, "jobs": 40} # useful for HW6
+stimulus_project
