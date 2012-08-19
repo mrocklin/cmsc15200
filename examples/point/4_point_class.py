d2983d397b3868170917ff7b18c9e59ba9d1c929
Matthew Rocklin <mrocklin@gmail.com>
2012-08-19 13:27:31 -0500

    add __str__ to point class

--- examples/point/4_point_class.py
+++ examples/point/4_point_class.py
@@ -8,8 +8,15 @@ class Point(object):
     def distTo(self, other):
         return sqrt( (self.x - other.x)**2 + (self.y - other.y)**2 )
 
+    def __str__(self): # Convert to string function
+        return "(%f, %f)"%(self.x, self.y)
+
 p = Point(3, 2)
 q = Point(1, 0)
 
 print("The distance between the points (%f, %f) and (%f, %f) is %f"%
         (p.x, p.y, q.x, q.y, p.distTo(q)))
+
+# Or the following
+print("The distance between the points %s and %s is %f"%
+        (str(p), str(q), p.distTo(q)))
