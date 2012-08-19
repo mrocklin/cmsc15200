d9dba2b5f923a5bfed6a133c648f668b52d66ef0
Matthew Rocklin <mrocklin@gmail.com>
2012-08-19 13:20:26 -0500

    correct spacing

--- examples/point/3_point_dict.py
+++ examples/point/3_point_dict.py
@@ -1,9 +1,9 @@
 from math import sqrt
-p = {"x": 3, "y":2}
-q = {"x": 1, "y":0}
+p = {"x": 3, "y": 2}
+q = {"x": 1, "y": 0}
 
 def dist(a, b):
     return sqrt( (a["x"] - b["x"])**2 + (a["y"] - b["y"])**2 )
 
 print("The distance between the points (%f, %f) and (%f, %f) is %f"%
-    (p['x'], p['y'], q['x'], q['y'], dist(p, q)))
+        (p['x'], p['y'], q['x'], q['y'], dist(p, q)))
